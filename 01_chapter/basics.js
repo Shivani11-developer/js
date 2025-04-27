5aed00269797345c3323544f0e721bc690e935de
Shivani11-developer <shivanikccpl10@gmail.com>
2025-04-27 18:22:29 +0000

    add new function

--- 01_chapter/basics.js
+++ 01_chapter/basics.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+console.log("hello world");
+function sample(){
+ const studentId = 45;
+ let studentName = 'Priya';
+ let subjects = [
+    "English",
+    "Hindi",
+    "Science"
+  ];
+  console.table([studentId,studentName,subjects,nextSubjects]);
+}
+sample();
+
